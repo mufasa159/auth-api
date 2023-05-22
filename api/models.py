c27e0d8b4b6f123a4c08526f7e5983b4076c5761
mufasa159 <mufasa159@proton.me>
2023-05-22 14:54:57 -0400

    remove: unncessary models

--- api/models.py
+++ api/models.py
@@ -6,36 +6,6 @@ class Role(Enum):
    admin = "admin"
    maintainer = "maintainer"
    user = "user"
-
-
-class Account(BaseModel):
-   uid: str
-   name_first: str
-   name_last: str
-   image: str | None = None
-   role: Role
-   username: str
-   email: str
-   hash: str
-   bio: str | None = None
-   created_at: str
-   last_login_at: str | None = None
-   last_login_ip: str | None = None
-   current_login_at: str | None = None
-   current_login_ip : str | None = None
-   login_count: int
-   confirmed_email: bool
-
-
-class TokenData(BaseModel):
-   id: int
-   uid: str
-   access_token: str
-   refresh_token: str
-   created_at: str
-   pair_count: int
-   refresh_token_use_count: int
-   refresh_token_last_used_at: str | None = None
  
 
 class NewUser(BaseModel):
