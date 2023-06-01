b3c4df3a2581a2c2c8dd277329cedec2ff2fa117
mufasa159 <mufasa159@proton.me>
2023-06-01 16:00:52 -0400

    add: token expiration and profile update config

--- api/config.py
+++ api/config.py
@@ -1,10 +1,15 @@
 from pydantic import BaseSettings
 
+
 class Configuration(BaseSettings):
-   domain: str = "https://localhost:8000"
-   active_mail_service: bool = False
-   access_token_expiration_minute: int = 60
-   environment: str = "development"
+   domain: str                          = "https://localhost:8000"
+   active_mail_service: bool            = False
+   access_token_expiration_day: int     = 0
+   access_token_expiration_minute: int  = 60
+   refresh_token_expiration_day: int    = 7
+   refresh_token_expiration_minute: int = 0
+   allow_username_change: bool          = False
+   environment: str                     = "development"
 
 
 settings = Configuration()
